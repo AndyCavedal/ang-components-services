005d76deb22cc1350b97c8bdab3b67f633be9484
Andy <hcavedal@gmail.com>
2023-10-09 23:24:44 -0300

    [add] directives, and RXJS

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,15 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StoreService } from '../../services/store.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
-export class NavComponent {
+export class NavComponent implements OnInit{
 
   showMenu: boolean = false;
+  counter = 0;
 
-  // toggleMenu() {
-  //   this.showMenu = !this.showMenu;
-  // }
+constructor(
+  private storeService: StoreService
+) {}
+
+ngOnInit(): void {
+  this.storeService.myCart$.subscribe(products => {
+    this.counter = products.length;
+  })
+}
+
 }
